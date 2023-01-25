77a171d6d8dcf4ffef4d1a654db0e98da91cc7a1
Marin <marin@dev.nddesign.no>
2023-01-25 16:28:25 +0200

    configure method

--- src/Database/Seeders/MarinarUsersRemoveSeeder.php
+++ src/Database/Seeders/MarinarUsersRemoveSeeder.php
@@ -9,10 +9,13 @@
 
         use \Marinar\Marinar\Traits\MarinarSeedersTrait;
 
-        public function run() {
-            if(!in_array(env('APP_ENV'), ['dev', 'local'])) return;
+        public static function configure() {
             static::$packageName = 'marinar_users';
             static::$packageDir = MarinarUsers::getPackageMainDir();
+        }
+
+        public function run() {
+            if(!in_array(env('APP_ENV'), ['dev', 'local'])) return;
 
             $this->autoRemove();
 
