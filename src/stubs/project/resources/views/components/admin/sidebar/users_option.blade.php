82b21faf150f737a224282e667a869bfcb3c3798
Marin <marin@dev.nddesign.no>
2023-02-02 17:17:25 +0200

    to not see sidebar option if do not have view right

--- src/stubs/project/resources/views/components/admin/sidebar/users_option.blade.php
+++ src/stubs/project/resources/views/components/admin/sidebar/users_option.blade.php
@@ -1,7 +1,9 @@
-{{--   USERS --}}
-<li class="nav-item @if(request()->route()->named("{$whereIam}.users.*")) active @endif">
-    <a class="nav-link " href="{{route("{$whereIam}.users.index")}}">
-        <i class="fa fa-fw fa-users mr-1"></i>
-        <span>@lang("admin/users/users.sidebar")</span>
-    </a>
-</li>
+@if($authUser->can('view', \App\Models\User::class))
+    {{--   USERS --}}
+    <li class="nav-item @if(request()->route()->named("{$whereIam}.users.*")) active @endif">
+        <a class="nav-link " href="{{route("{$whereIam}.users.index")}}">
+            <i class="fa fa-fw fa-users mr-1"></i>
+            <span>@lang("admin/users/users.sidebar")</span>
+        </a>
+    </li>
+@endif
