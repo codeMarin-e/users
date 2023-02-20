18071a618ad3c2a3ff98ba84caf4afe88bda2cc4
Marin <marin@dev.nddesign.no>
2023-02-20 10:22:15 +0200

    new hooks

--- src/stubs/project/config/marinar_users.php
+++ src/stubs/project/config/marinar_users.php
@@ -32,6 +32,7 @@ return [
      * If path is file - only it
      */
     'exclude_stubs' => [
+        // @HOOK_CONFIG_EXCLUDE_STUBS
     ],
 
     /**
@@ -39,7 +40,9 @@ return [
      * file_path => [ '@hook1', '@hook2' ]
      * file_path => * - all from this file
      */
-    'exclude_injects' => [],
+    'exclude_injects' => [
+        // @HOOK_CONFIG_EXCLUDE_INJECTS
+    ],
 
     /**
      * Addons hooked to the package
